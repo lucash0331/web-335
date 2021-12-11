fdfc6c9476694a1dc13b84b1e00d2dd327748b78
Lucas Hoffman <75821031+lucash0331@users.noreply.github.com>
2021-12-11 18:07:25 -0500

    Create hoffman_calculator.py

---
+++ week-8/hoffman_calculator.py
@@ -0,0 +1,26 @@
+#
+#=====================================================
+# Title: Assignment 8.3 
+# Author: Lucas Hoffman
+# Date: December 11, 2021
+# Description: Basic math operations using Python
+#=====================================================
+#
+
+#addition function
+def add(number01, number02):
+    return number01 + number02
+
+#subtraction function
+def subtract(number01, number02):
+    return number01 - number02
+
+#division function
+def divide(number01, number02):
+    return number01/number02
+
+
+#outputs
+print(add(1,2))
+print(subtract(4,1))
+print(divide(8,2))
